98d485a9ede00ff5c04a290c4999c0db5bdb1c4f
Yomar Miranda <mirandayomar@gmail.com>
2018-06-28 13:33:38 -0500

    Implementación de Rutas y Nuevo en la App.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Square';
-  ciudades: any = [
-    { nombre: "Barcelona", poblacion: 2500000, cercania: 1, distancia: 5, publicidad: true },
-    { nombre: "Berlin", poblacion: 3700000, cercania: 1, distancia: 8, publicidad: true },
-    { nombre: "Cartagena", poblacion: 1600000, cercania: 1, distancia: 3, publicidad: false },
-    { nombre: "Buenos Aires", poblacion: 2500000, cercania: 2, distancia: 14, publicidad: true },
-    { nombre: "Santiago de Chile", poblacion: 3700000, cercania: 2, distancia: 11, publicidad: false },
-    { nombre: "Ciudad de Mexico", poblacion: 11600000, cercania: 2, distancia: 19, publicidad: false },
-    { nombre: "Los Angeles", poblacion: 3400000, cercania: 3, distancia: 28, publicidad: true },
-    { nombre: "Santorini", poblacion: 3700000, cercania: 3, distancia: 25, publicidad: true },
-    { nombre: "Rio de Janeiro", poblacion: 7800000, cercania: 3, distancia: 31, publicidad: false }
-  ];
-  lat: number = 4.6293029;
-  lng: number = -74.1747479;
-  constructor(){
-    
-  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,31 @@ import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { ResaltarDirective } from './directives/resaltar.directive';
 import { ContarClicksDirective } from './directives/contar-clicks.directive';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DetalleComponent } from './detalles/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
 
 const appRoutes: Routes = [
-  { path: '', component: AppComponent },
-  { path: 'lugares', component: AppComponent }
+  { path: '', component: LugaresComponent },
+  { path: 'lugares', component: LugaresComponent },
+  { path: 'detalle', component: DetalleComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ResaltarDirective,
-    ContarClicksDirective
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBblSu-nxTi5l5Ns9ND6Ynjz901CXDXS-g'
-    })
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
