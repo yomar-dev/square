3c070b9cd38dba7f65f24b055eeaa58683781d0e
Yomar Miranda <mirandayomar@gmail.com>
2018-06-28 01:13:15 -0500

    Directiva - ngIf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Square';
   ciudades:any = [
-    { nombre: "Barcelona" },
-    { nombre: "Berlin" },
-    { nombre: "Cartagena" }
+    { nombre: "Barcelona", poblacion: 2500000 },
+    { nombre: "Berlin", poblacion: 3700000 },
+    { nombre: "Cartagena", poblacion: 1600000 },
+    { nombre: "Buenos Aires", poblacion: 2500000 },
+    { nombre: "Santiago de Chile", poblacion: 3700000 },
+    { nombre: "Ciudad de Mexico", poblacion: 11600000 },
+    { nombre: "Los Angeles", poblacion: 3400000 },
+    { nombre: "Santorini", poblacion: 3700000 },
+    { nombre: "Rio de Janeiro", poblacion: 7800000 }
   ]
   constructor(){
     
