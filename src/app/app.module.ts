cd85e122fea732297da16130cc8ad839125cf8d4
Yomar Miranda <mirandayomar@gmail.com>
2018-06-28 21:59:29 -0500

    Página de contacto para Square.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { ContarClicksDirective } from './directives/contar-clicks.directive';
 import { Routes, RouterModule } from '@angular/router';
 import { DetalleComponent } from './detalles/detalle.component';
 import { LugaresComponent } from './lugares/lugares.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 const appRoutes: Routes = [
   { path: '', component: LugaresComponent },
   { path: 'lugares', component: LugaresComponent },
-  { path: 'detalle/:id', component: DetalleComponent }
+  { path: 'detalle/:id', component: DetalleComponent },
+  { path: 'contacto', component: ContactoComponent }
 ]
 
 @NgModule({
@@ -22,7 +24,8 @@ const appRoutes: Routes = [
     ResaltarDirective,
     ContarClicksDirective,
     DetalleComponent,
-    LugaresComponent
+    LugaresComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
